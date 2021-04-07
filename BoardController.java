df42c2838d824fe005fe46c8c20746bcb7165438
kanae2011 <62246184+kanae2011@users.noreply.github.com>
2021-04-07 17:13:59 +0900

    Update BoardController.java

--- BoardController.java
+++ BoardController.java
@@ -1,12 +1,13 @@
 package org.zerock.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.board.service.BoardService;
-import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Controller
@@ -19,17 +20,21 @@ public class BoardController {
 	//@Setter-lombok사용, @Autowired-spring 사용
 	//대신 사용 가능한 어노테이션 : @Autowired - spring,@inject - java
 	//DI 적용시 BoardService 타입 : 1. BoardService interface 2.BoardServiceImpl class
-	@Setter(onMethod_ = @Autowired )
+//	@Setter(onMethod_ = @Autowired )
+	@Autowired
+	// BoardService 상속 받아서 타입이 같은 것이 있으면 어떤 것을 넣어 줄지 결정이 안돼서 오류 - 해결
+	@Qualifier("bsi")
 	private BoardService service;
 	
 	//실행할 메소드 - list
 	//맵핑 -get방식/ list.do
 	@GetMapping("/list.do")
-	public String list() throws Exception {
+	public String list(Model model) throws Exception {
 		
 		log.info("list() - board List");
 		
-		service.list();
+		//model에 데이터를 담으면 model안에 있는 request에 데이터가 담김 
+		model.addAttribute("list",service.list());
 		
 		return MODULE +  "/list";
 	}
