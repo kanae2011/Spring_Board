a7c96b5c0e999a6f1412380deddaa4ebb4ad5779
kanae2011 <62246184+kanae2011@users.noreply.github.com>
2021-04-07 17:14:55 +0900

    Update BoardServiceImpl.java

--- BoardServiceImpl.java
+++ BoardServiceImpl.java
@@ -3,8 +3,10 @@ package org.zerock.board.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.zerock.board.dao.BoardDAO;
+import org.zerock.board.mapper.BoardMapper;
 import org.zerock.board.vo.BoardVO;
 
 import lombok.Setter;
@@ -16,6 +18,8 @@ import lombok.extern.log4j.Log4j;
 
 @Service
 @Log4j
+//BoardService 상속 받아서 같은 것이 있으면 어떤 것을 넣어 줄지 결정이 안돼서 오류 발생->  @Qualifier 지정 
+@Qualifier("bsi")
 public class BoardServiceImpl implements BoardService{
 	
 	
@@ -23,15 +27,16 @@ public class BoardServiceImpl implements BoardService{
 	//대신 사용 가능한 어노테이션 : @Autowired - spring,@inject - java
 	//DI 적용시 BoardService 타입 : 1. BoardService interface 2.BoardServiceImpl class
 	@Setter(onMethod_ = @Autowired )
-	private BoardDAO dao;
+//	private BoardDAO dao;
+	private BoardMapper mapper;
 
 	@Override
 	public List<BoardVO> list() throws Exception {
 		// TODO Auto-generated method stub
 		log.info("list()-BoardListService실행");
-		dao.list();
-		dao.getTotalRow();
-		return null;
+//		dao.list();
+//		dao.getTotalRow();
+		return mapper.list();
 	}
 
 	@Override
