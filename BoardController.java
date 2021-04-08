cb52bc77c1dfffffac78f910400e33c28ff43ebe
kanae2011 <62246184+kanae2011@users.noreply.github.com>
2021-04-08 15:10:13 +0900

    Update BoardController.java

--- BoardController.java
+++ BoardController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.board.service.BoardService;
+import org.zerock.board.vo.BoardVO;
+
 import lombok.extern.log4j.Log4j;
 
 @Controller
@@ -39,11 +41,20 @@ public class BoardController {
 		return MODULE +  "/list";
 	}
 	
+	//실행할 메소드 -view
+	//맵핑 -get방식/ view.do
+	@GetMapping("/view.do")
+	public String view(Model model, Long no) throws Exception {
+		
+		log.info("view().no: " + no + " - board View");
+		model.addAttribute("vo",service.view(no));
+		return MODULE + "/view";
+	}
 	
 	//실행할 메소드 -writeForm
 	//맵핑 -get방식/ write.do
 	@GetMapping("/write.do")
-	public String writeForm() {
+	public String writeForm() throws Exception{
 		
 		log.info("writeForm() - board Write Form");
 		return MODULE + "/write";
@@ -52,45 +63,44 @@ public class BoardController {
 	//실행할 메소드 -write 처리
 	//맵핑 -post방식/ write.do
 	@PostMapping("/write.do")
-	public String write() {
+	public String write(BoardVO vo) throws Exception{
 		
-		log.info("write() - board Write");
+		log.info("write().vo: " + vo + " - board Write");
+		service.write(vo);
 		return "redirect:list.do";
 	}
 	
-	//실행할 메소드 -view
-	//맵핑 -get방식/ view.do
-	@GetMapping("/view.do")
-	public String view() {
-		
-		log.info("view() - board View");
-		return MODULE + "/view";
-		}
 		
 	//실행할 메소드 -updateForm
 	//맵핑 -get방식/ update.do
 	@GetMapping("/update.do")
-	public String updateForm() {
+	public String updateForm(Model model,Long no) throws Exception{
 		
 		log.info("updateForm() - board Update Form");
+		
+		model.addAttribute("vo",service.view(no));
+		
 		return MODULE + "/update";
 		}
 		
 	//실행할 메소드 -update 처리
 	//맵핑 -post방식/ update.do
 	@PostMapping("/update.do")
-	public String update() {
+	public String update(BoardVO vo) throws Exception{
 		
-		log.info("write() - board Update");
-		return "redirect:view.do";
+		log.info("update().vo : " + vo + " - board Update");
+		service.update(vo);
+		return "redirect:view.do?no=" + vo.getNo();
 		}
 		
 	//실행할 메소드 -delete
 	//맵핑 -get방식/ delete.do
 	@GetMapping("/delete.do")
-	public String deleteForm() {
+	public String deleteForm(Long no) throws Exception{
 		
-		log.info("delete() - board Delete Form");
+		log.info("delete().no" + no + " - board Delete Form");
+		service.delete(no);
+		
 		return "redirect:list.do";
 		}
 	
